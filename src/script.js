b5a9f8375ffd2ac9d4250ec0212dd03477aaec52
ma1aise <lisaewells03@outlook.com>
2023-09-28 15:32:16 +1000

    Editing weekly forecast

--- src/script.js
+++ src/script.js
@@ -19,8 +19,34 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[dayIndex];
-
   return `${day} ${hours}:${minutes}`;
+}
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/01d@2x.png"
+                  alt=""
+                  width="36"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max"> 18° </span>
+                  <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+              </div>
+              `;
+  });
+
+  forecastHTML + forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 // Display the Temperature, Humidity, Wind Speed and the Cloud Coverage
@@ -119,3 +145,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 searchCity("Madrid");
+displayForecast();
